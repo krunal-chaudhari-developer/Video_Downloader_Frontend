d03edc55f09ff23742f48c0eba6a800516329d51
krunal-chaudhari-developer <chaudharikrunal825@gmail.com>
2023-10-16 13:27:56 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./components/Layout/Navbar/Navbar";
+
+const App = () => {
+  return (
+    <>
+      <div className="">
+        <Navbar />
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/data.js
@@ -0,0 +1,328 @@
+import image1 from "./Facebook/facebook images/feature1.png";
+import image2 from "./Facebook/facebook images/rty 1.png";
+import image3 from "./Facebook/facebook images/My project 1.png";
+import image4 from "./Facebook/facebook images/My project (1) 1.png";
+import image5 from "./Instagram/instagram images/image1.png";
+import image6 from "./Instagram/instagram images/image2.png";
+import image7 from "./Instagram/instagram images/image3.png";
+import image8 from "./Instagram/instagram images/image4.png";
+import image9 from "./Instagram/instagram images/image5.png";
+import image10 from "./Linkedin/linkedin images/image1.png";
+import image11 from "./Linkedin/linkedin images/image2.png";
+import image12 from "./Pinterest/pinterest images/image1.png";
+import image13 from "./Pinterest/pinterest images/image2.png";
+import image14 from "./Soundcloud/soundcloud images/Image.png";
+import image15 from "./Snapchat/snapchat images/image1.png";
+import image16 from "./Snapchat/snapchat images/image2.png";
+import image17 from "./Snapchat/snapchat images/image3.png";
+import image18 from "./Snapchat/snapchat images/image4.png";
+import image20 from "./Twitter/twitter images/image2.png";
+import image19 from "./Twitter/twitter images/image1.png";
+import image21 from "./Twitter/twitter images/image3.png";
+import image22 from "./Vimeo/vimeo images/image1.png";
+import image23 from "./Vimeo/vimeo images/image2.png";
+import image24 from "./Vimeo/vimeo images/image3.png";
+import image25 from "./Youtube/youtube images/Image 1.jpg";
+import image26 from "./Youtube/youtube images/Image 2.png";
+import image27 from "./Youtube/youtube images/Image 3.png";
+import image28 from "./Youtube/youtube images/Image 4.png";
+
+export const facebookSlides = [
+  {
+    title: "Photos Downloader",
+    className: "feature1",
+    description:
+      "Facebook Photo Downloader from inlifa allows you to save any photo or collage from Facebook without any difficulty. With inlifa you can download a single post image as well as download multiple Facebook photos.",
+    images: image1,
+  },
+  {
+    title: "Videos Downloader",
+    className: "feature2",
+    description:
+      "inlifa is created to enable you to download videos for any purpose you want. inlifa supports video downloading for singular videos and multiple videos from carousels.",
+    images: image2,
+  },
+  {
+    title: "Reels Downloader",
+    className: "feature3",
+    description:
+      "Reels is a new video format that clones the principle of Facebook. Download Facebook Reels videos with the help of inlifa. Our Facebook Reel downloader can help you to save your favorite Reels videos.",
+    images: image3,
+  },
+  {
+    title: "Facebook Story Download",
+    className: "feature4",
+    description:
+      "Facebook stories are a unique way for anyone to share their everyday life with photos and videos with everyone. Sometimes you want to archive a story you like but the feature is not supported in the app. Snapinsta was created to solve that problem; to help people download Instagram stories and watch them online.",
+    images: image4,
+  },
+];
+
+export const instaSlides = [
+  {
+    title: "Photos Downloader",
+    className: "feature11",
+    description:
+      "Instagram Photo Downloader from inlifa allows you to save any photo or collage from Instagram without any difficulty. With inlifa you can download a single post image as well as download multiple Instagram photos.",
+    images: image5,
+  },
+  {
+    title: "Videos Downloader",
+    className: "feature21",
+    description:
+      "inlifa is created to enable you to download videos for any purpose you want. inlifa supports video downloading for singular videos and multiple videos from carousels.",
+    images: image6,
+  },
+  {
+    title: "IGTV Downloader",
+    className: "feature31",
+    description:
+      "IGTV is a long video type. If you can’t watch it now, you can download IGTV videos to your device to be sure that you can return to watching later, without the need to be online or in case the IGTV can be deleted.",
+    images: image7,
+  },
+  {
+    title: "Reels Downloader",
+    className: "feature41",
+    description:
+      "Reels is a new video format that clones the principle of Instagram. Download Instagram Reels videos with the help of inlifa. Our Instagram Reel downloader can help you to save your favorite Reels videos.",
+    images: image8,
+  },
+  {
+    title: "Instagram Story Download",
+    className: "feature51",
+    description:
+      "Instagram stories are a unique way for anyone to share their everyday life with photos and videos with everyone. Sometimes you want to archive a story you like but the feature is not supported in the app. inlifa was created to solve that problem; to help people download Instagram stories and watch them online.",
+    images: image9,
+  },
+];
+
+export const linkedinSlides = [
+  {
+    title: "Photos Downloader",
+    className: "feature18",
+    description:
+      "linkedin Photo Downloader from inlifa allows you to save any photo or collage from Instagram without any difficulty. With inlifa you can download a single post image as well as download multiple Instagram photos.",
+    images: image10,
+  },
+  {
+    title: "Videos Downloader",
+    className: "feature28",
+    description:
+      "inlifa is created to enable you to download LinkedIn videos for any purpose you want. inlifa supports video downloading for singular videos and multiple videos from carousels.",
+    images: image11,
+  },
+];
+
+export const pinterestSlides = [
+  {
+    title: "Photos Downloader",
+    className: "feature15",
+    description:
+      "Pinterest Photo Downloader from Snapinsta allows you to save any photo or collage from Pinterest without any difficulty. With Snapinsta you can download a single post image as well as download multiple Pinterest photos.",
+    images: image12,
+  },
+  {
+    title: "Videos Downloader",
+    className: "feature25",
+    description:
+      "inlifa is created to enable you to download IG videos for any purpose you want. inlifa supports video downloading for singular videos and multiple videos from carousels.",
+    images: image13,
+  },
+];
+
+export const soundcloudSlides = [
+  {
+    title: "SoundCloud Music Downloader",
+    className: "feature13",
+    description:
+      "Inlifa was created to allow you to Download the songs from SoundCloud App. Inlifa supports downloading SoundCloud music for some Songs and many Songs from SoundCloud.",
+    images: image14,
+  },
+];
+
+export const snapchatSlides = [
+  {
+    title: "Photos Downloader",
+    className: "feature17",
+    description:
+      "Snapchat Photo Downloader from inlifa allows you to save any photo or collage from snapchat without any difficulty. With inlifa you can download a single post image as well as download multiple Snapchhat photos.",
+    images: image15,
+  },
+  {
+    title: "Videos Downloader",
+    className: "feature27",
+    description:
+      "inlifa is created to enable you to download IG videos for any purpose you want. inlifa supports video downloading for singular videos and multiple videos from carousels.",
+    images: image16,
+  },
+  {
+    title: "Spotlight Downloader",
+    className: "feature37",
+    description:
+      "Spotlight is a new video format that clones the principle of . Download Snapchat Reels videos with the help of inlifa. Our Instagram Reel downloader can help you to save your favorite Reels videos.",
+    images: image17,
+  },
+  {
+    title: "Snapchat Story Downloader",
+    className: "feature47",
+    description:
+      "Snapchatstories are a unique way for anyone to share their everyday life with photos and videos with everyone. Sometimes you want to archive a story you like but the feature is not supported in the app. Snapinsta was created to solve that problem; to help people download Instagram stories and watch them online.",
+    images: image18,
+  },
+];
+
+export const twitterSlides = [
+  {
+    title: "Photos Downloader",
+    className: "feature16",
+    description:
+      "Twitter Photo Downloader from inlifa allows you to save any photo or collage from Instagram without any difficulty. With inlifa you can download a single post image as well as download multiple Twitter photos.",
+    images: image19,
+  },
+  {
+    title: "Videos Downloader",
+    className: "feature26",
+    description:
+      "inlifa is created to enable you to download videos for any purpose you want. inlifa supports video downloading for singular videos and multiple videos from carousels.",
+    images: image20,
+  },
+  {
+    title: "GIF Downloader",
+    className: "feature36",
+    description:
+      "GIF is a long video type. If you can’t watch it now, you can download GIF videos to your device to be sure that you can return to watching later, without the need to be online or in case the GIF can be deleted.",
+    images: image21,
+  },
+];
+
+export const vimeoSlides = [
+  {
+    title: "Vimeo to mp4 in just one click!",
+    className: "feature14",
+    description:
+      "Inlifa's Vimeo online video downloader lets you download videos straight from the Vimeo platform in one click. Your video will be exported in mp4 format so you can play it on any media player or share on different social media or video-sharing sites. No watermarks so you can repurpose and share them anywhere. No loss in video quality—download videos in HD.",
+    images: image22,
+  },
+  {
+    title: "Convert vimeo videos to mp3 or create a new video",
+    className: "feature24",
+    description:
+      "You can even download a Vimeo video as an audio file! Just click on “Open in editor” once your video is ready and export it as an mp3 from the download options. Or—create a new music video! You can add sound effects and background music; choose from our library of stock audio and video. Our premium subscribers get full access to our stock audio media library. Check our pricing page for more info.",
+    images: image23,
+  },
+  {
+    title: "All your video editing needs in one place",
+    className: "feature34",
+    description:
+      "Inlifa lets you do it all in one place! It features all the video editing tools you will ever need to create amazing videos that you can share with the world. Make your videos look professional in just a few clicks—even without any video editing experience. Add subtitles to your videos to make them accessible for everyone. Record your screen and webcam to create tutorial videos, add animated text, images, remove background from video, and more!",
+    images: image24,
+  },
+];
+
+export const youtubeSlides = [
+  {
+    title: "Youtube Video Downloader to Mp3",
+    className: "feature12",
+    description:
+      "Inlifa was created to allow you to Download your own content from Youtube. Inlifa supports downloading Youtube Videos to mp3 for some videos and many videos from Youtube.",
+    images: image25,
+  },
+  {
+    title: "Download Youtube Shorts to Mp3",
+    className: "feature22",
+    description:
+      "Youtube Shorts to mp3 Downloader by inlifa will help you download Shorts videos to mp3 from Youtube posts to your device in the easiest way when Youtube does not support you in this.",
+    images: image26,
+  },
+  {
+    title: "Youtube Video to Mp3",
+    className: "feature32",
+    description:
+      "You can download the Youtube Video to mp3 in inlifa. You have to copy the link as same as given in picture and paste it in search bar.",
+    images: image27,
+  },
+  {
+    title: "Photos Downloader",
+    className: "feature42",
+    description:
+      "Youtube Photo Downloader from inlifa allows you to save any photo or collage from Youtube without any difficulty. With inlifa you can download a single post image as well as download multiple Youtube photos.",
+    images: image28,
+  },
+];
+
+export const homepageContent = [
+  {
+    title: "Facebook Video Download",
+    className: "fab fa-facebook",
+    description:
+      "In this Website you can download the Videos from Stories, Reels and from Posts",
+  },
+  {
+    title: "Instagram Video & Photo Download",
+    className: "fa fa-instagram",
+    description:
+      "This Website is created for high quality videos from Instagram. You can download the videos from Stories, Reels, and From Posts.",
+  },
+  {
+    title: "Youtube Video Download",
+    className: "fa fa-youtube",
+    description:
+      "This universal Website is created to download the videos from youtube in many formats and You can also download the videos in Audio.",
+  },
+  {
+    title: "Soundcloud Song Downloader",
+    className: "fa fa-soundcloud",
+    description:
+      " This universal soundcloud downloader created to download the songs from the soundcloud website by copying link and download in mp3.",
+  },
+  {
+    title: "Pinterest Video Download",
+    className: "fa fa-pinterest",
+    description:
+      "This universal website is created to download the videos from Pinterest. You can download the Videos from pinterest at very good quality.",
+  },
+  {
+    title: "Vimeo Video Download",
+    className: "fa fa-vimeo",
+    description:
+      "This universal website is created to download the Videos from Vimeo at Very good Quality.",
+  },
+  {
+    title: "Snapchat Video Download",
+    className: "fa fa-snapchat",
+    description:
+      "This universal website is created to download the spotlights from snapchat.",
+  },
+  {
+    title: "Linkedin Video Download",
+    className: "fab fa-linkedin",
+    description:
+      "This universal website is created to download the Videos from Linkedin.",
+  },
+];
+
+export const homepageContent1 = [
+  {
+    title: "For content curation",
+    description:
+      " Lots of Facebook videos are really worthy of being saved forever on your device to be used as part of a blogger review,or a journalist documentary. And these platforms don’t offer saving of videos directly in the app or the website — so it is better to use the special tool for this goal.So you can bookmark this page and always have a Facebook video downloader on hand. Facebook is not such a popular social media platform to save from. But it also has a large amount of worthy videos on different topics that are really great to save on your device for the future — especially when we are looking for user-generated content for your brand.",
+  },
+  {
+    title: "To have content for future memories",
+    description:
+      " People download Instagram photos to keep for future memories. They are making albums with various photos of their friends, or albums for events — this is their insurance that all the treasures will stay forever in their pocket.",
+  },
+];
+
+export const homepageContent2 = [
+  {
+    title: "For repurposing content",
+    description:
+      "Not the least popular reason to save Instagram photos is to reuse them in the original quality for repurposing content. For instance, you can reuse the content for your professional goals, for design reasons, as a part of your art, etc",
+  },
+  {
+    title: "To save something forever",
+    description:
+      "This kind of tool is quite popular in 2023 as people want to keep content on their device memory forever. It doesn’t as safe to keep everything just on the social media platform. In this era of the internet we all understand how fragile social media platforms are. And how your data are unprotected. It is better to have a comfortable and simple tool to save everything you need right on your device and have it in your hand now.",
+  },
+];
